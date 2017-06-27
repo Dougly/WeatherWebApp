15330f531406c8071d93c6488c9e07bb95fa0488
Dougly <dgalante11@gmail.com>
2017-06-27 12:12:44 -0400

    alert prints lat long

--- Model/GoogleMapsAPI.js
+++ Model/GoogleMapsAPI.js
@@ -1,14 +1,10 @@
 
 
 function initMap() {
-  var map = new google.maps.Map(document.getElementById('map'), {
-  zoom: 8,
-  center: {lat: -34.397, lng: 150.644}
-  });
   var geocoder = new google.maps.Geocoder();
 
   document.getElementById('submit').addEventListener('click', function() {
-  geocodeAddress(geocoder, map);
+    geocodeAddress(geocoder);
   });
 }
 
@@ -17,12 +13,6 @@ function geocodeAddress(geocoder, resultsMap) {
   geocoder.geocode({'address': address}, function(results, status) {
     if (status === 'OK') {
       alert(results[0].geometry.location);
-      resultsMap.setCenter(results[0].geometry.location);
-      var marker = new google.maps.Marker({
-      map: resultsMap,
-      position: results[0].geometry.location
-      });
-
     } else {
       alert('Geocode was not successful for the following reason: ' + status);
     }
