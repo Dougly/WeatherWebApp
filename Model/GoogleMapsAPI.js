1fa997255e8083f7fc33714a92c4c5282c8698a8
Dougly <dgalante11@gmail.com>
2017-06-27 12:47:27 -0400

    getting weather data for entered city

--- Model/GoogleMapsAPI.js
+++ Model/GoogleMapsAPI.js
@@ -1,4 +1,4 @@
-
+var test = "hello";
 
 function initMap() {
   var geocoder = new google.maps.Geocoder();
@@ -12,9 +12,36 @@ function geocodeAddress(geocoder, resultsMap) {
   var address = document.getElementById('address').value;
   geocoder.geocode({'address': address}, function(results, status) {
     if (status === 'OK') {
-      alert(results[0].geometry.location);
+      getWeatherData(results[0].geometry.location);
     } else {
       alert('Geocode was not successful for the following reason: ' + status);
     }
+  });
+}
+
+function getWeatherData(location) {
+  var lat = location.lat();
+  var lng = location.lng();
+  var key = "442cb8009c262b65928a26aa8f86c66b"
+  var url = "https://api.darksky.net/forecast/" + key + "/" + lat + "," + lng + "?callback=?"
+
+  $(function() {
+
+    $.getJSON(url, function(data) {
+      console.log(data);
+    });
+
+    // $.ajax({
+    //   type: 'GET',
+    //   url: url,
+    //   success: function(data) {
+    //     console.log("HI");
+    //     console.log(data);
+    //   },
+    //   error: function() {
+    //     alert("Could not get weather data");
+    //   }
+    // });
+
   });
 }
